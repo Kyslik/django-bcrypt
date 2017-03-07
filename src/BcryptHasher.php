6a7fd964e1fd8fd0a6a68e1ca9384bf061ae57c5
Martin Kiesel <martin.kiesel@gmail.com>
2017-03-07 09:35:12 +0100

    Update needs rehash function
      -  needs rehash returns true if prefix can not be found

--- src/BcryptHasher.php
+++ src/BcryptHasher.php
@@ -5,6 +5,7 @@ use Illuminate\Hashing\BcryptHasher as OriginalHasher;
 
 class BcryptHasher extends OriginalHasher
 {
+
     /**
      * Default crypt cost factor (django default).
      *
@@ -12,29 +13,32 @@ class BcryptHasher extends OriginalHasher
      */
     protected $rounds = 12;
 
-
     protected $prefix = 'bcrypt_sha256$';
+
 
     /**
      * Hash the given value.
      *
-     * @param  string  $value
-     * @param  array   $options
+     * @param  string $value
+     * @param  array  $options
+     *
      * @return string
      *
      * @throws \RuntimeException
      */
     public function make($value, array $options = []): string
     {
-        return $this->prefix . parent::make(hash('sha256', $value), $options);
+        return $this->prefix.parent::make(hash('sha256', $value), $options);
     }
+
 
     /**
      * Check the given plain value against a hash.
      *
-     * @param  string  $value
-     * @param  string  $hashedValue
-     * @param  array   $options
+     * @param  string $value
+     * @param  string $hashedValue
+     * @param  array  $options
+     *
      * @return bool
      */
     public function check($value, $hashedValue, array $options = []): bool
@@ -46,24 +50,32 @@ class BcryptHasher extends OriginalHasher
         return password_verify(hash('sha256', $value), $this->removePrefix($hashedValue));
     }
 
-    /**
-     * Check if the given hash has been hashed using the given options.
-     *
-     * @param  string  $hashedValue
-     * @param  array   $options
-     * @return bool
-     */
-    public function needsRehash($hashedValue, array $options = []): bool
-    {
-        return parent::needsRehash($this->removePrefix($hashedValue), $options);
-    }
 
-    private function removePrefix(string $hashedValue): string {
+    private function removePrefix(string $hashedValue): string
+    {
         if (strpos($hashedValue, $this->prefix) !== false) {
             $hashedValue = substr($hashedValue, strlen($this->prefix));
         }
 
         return $hashedValue;
+    }
+
+
+    /**
+     * Check if the given hash has been hashed using the given options.
+     *
+     * @param  string $hashedValue
+     * @param  array  $options
+     *
+     * @return bool
+     */
+    public function needsRehash($hashedValue, array $options = []): bool
+    {
+        if (strpos($hashedValue, $this->prefix) === false) {
+            return true;
+        }
+
+        return parent::needsRehash($this->removePrefix($hashedValue), $options);
     }
 
 }
